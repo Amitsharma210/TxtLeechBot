78c6e15e6381b83259d34d298ce4488b7efcfd77
Amitsharma210 <144205854+Amitsharma210@users.noreply.github.com>
2024-06-07 12:10:32 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = "22164996"
+api_hash = "035f2448fefaf28659b05af05142b45b"
+bot_token = "7337099224:AAHeCHfuGGY8JqHuGcLafq6tIYdaJqW-7iA"
 
